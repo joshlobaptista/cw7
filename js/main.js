40123813c4e35c7efeb79624221c3a7976d5fad8
Joshua Baptista <joshua.lopesbaptista@gmail.com>
2018-07-04 23:17:40 -0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// Given a string s, write a method (function) that will return true if its a valid single integer or floating number or false if its not.
+
+
+function isDigit(s) {
+  //your code
+  return s==parseFloat(s)
+}
